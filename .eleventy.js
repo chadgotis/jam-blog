8f3eaa2dd135c4118619f26dfab37c58c38a1269
Chadric Gotis <chadric.gotis@abcopro.com.au>
2021-04-29 11:25:42 +0800

    re factored

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,4 @@
-module.exports = function (config) {
-  return {
-    dir: {
-      input: "src",
-      output: "dist",
-    },
-  };
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("admin");
 };
